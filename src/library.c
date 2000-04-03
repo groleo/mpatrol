57c63b7f18a2538d4207588086e9ed8330e4d331
graeme <graeme@6070c42c-5303-4300-a3c9-e5ce7133e722>
2000-04-03 18:04:41 +0000

    Renamed mpatrol.c to library.c

--- src/library.c
+++ src/library.c
@@ -34,7 +34,7 @@
 
 
 #if MP_IDENT_SUPPORT
-#ident "$Id: mpatrol.c,v 1.4 2000-03-23 19:58:27 graeme Exp $"
+#ident "$Id: library.c,v 1.1 2000-04-03 18:04:40 graeme Exp $"
 #endif /* MP_IDENT_SUPPORT */
 
 
