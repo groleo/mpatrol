ffd2bb2a242410d15dc2cf1f2e0b3c05cfb33840
graeme <graeme@6070c42c-5303-4300-a3c9-e5ce7133e722>
2001-02-08 19:48:29 +0000

    New file demonstrating __mp_snapshot() and __mp_iterate()

---
+++ tests/pass/test10.c
@@ -0,0 +1,121 @@
+/*
+ * mpatrol
+ * A library for controlling and tracing dynamic memory allocations.
+ * Copyright (C) 1997-2001 Graeme S. Roy <graeme@epc.co.uk>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free
+ * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307, USA.
+ */
+
+
+/*
+ * Demonstrates and tests the facility for obtaining information on
+ * local memory leaks.  Will also edit or list the location of each
+ * leak if the EDIT or LIST option is in effect.
+ */
+
+
+#include "mpatrol.h"
+#include <stdio.h>
+
+
+unsigned long leaktotal = 0;
+
+
+int callback(void *p)
+{
+    __mp_allocstack *s;
+    __mp_allocinfo d;
+
+    if (!__mp_info(p, &d))
+    {
+        if (sizeof(void *) == 8)
+            fprintf(stderr, "nothing known about address 0x%016lX\n", p);
+        else
+            fprintf(stderr, "nothing known about address 0x%08lX\n", p);
+        return -1;
+    }
+    if (!d.freed)
+    {
+        if (sizeof(void *) == 8)
+            fprintf(stderr, "0x%016lX", d.block);
+        else
+            fprintf(stderr, "0x%08lX", d.block);
+        fprintf(stderr, " %s", d.func ? d.func : "<unknown>");
+        fprintf(stderr, " %s", d.file ? d.file : "<unknown>");
+        fprintf(stderr, " %lu", d.line);
+        for (s = d.stack; s != NULL; s = s->next)
+        {
+            if (s == d.stack)
+                fputs(" (", stderr);
+            else
+                fputs("->", stderr);
+            if (s->name != NULL)
+                fprintf(stderr, "%s", s->name);
+            else if (sizeof(void *) == 8)
+                fprintf(stderr, "0x%016lX", s->addr);
+            else
+                fprintf(stderr, "0x%08lX", s->addr);
+            if (s->next == NULL)
+                fputc(')', stderr);
+        }
+        fputc('\n', stderr);
+        if ((d.file != NULL) && (d.line != 0))
+            __mp_view(d.file, d.line);
+        leaktotal += d.size;
+        return 1;
+    }
+    return 0;
+}
+
+
+void func2(unsigned long n)
+{
+    void *p;
+
+    p = malloc((n * 10) + 1);
+    if (n % 13)
+        free(p);
+}
+
+
+void func1(void)
+{
+    void *p;
+    size_t i, n;
+    unsigned long s;
+
+    p = malloc(16);
+    s = __mp_snapshot();
+    for (i = 0; i < 128; i++)
+        func2(i);
+    free(p);
+    if (n = __mp_iterate(callback, s))
+        fprintf(stderr, "Detected %lu memory leaks (%lu bytes)\n", n,
+                leaktotal);
+    if ((n != 10) || (leaktotal != 5860))
+        fputs("Expected 10 memory leaks (5860 bytes)\n", stderr);
+}
+
+
+int main(void)
+{
+    void *p;
+
+    p = malloc(16);
+    func1();
+    free(p);
+    return EXIT_SUCCESS;
+}
