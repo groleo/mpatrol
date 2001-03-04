e20060c59621c73c08213800ccb28c9a4a92708a
graeme <graeme@6070c42c-5303-4300-a3c9-e5ce7133e722>
2001-03-04 12:41:59 +0000

    Added the errordetails structure

--- src/diag.h
+++ src/diag.h
@@ -32,7 +32,7 @@
 
 
 /*
- * $Id: diag.h,v 1.26 2001-03-04 12:31:57 graeme Exp $
+ * $Id: diag.h,v 1.27 2001-03-04 12:41:59 graeme Exp $
  */
 
 
@@ -77,6 +77,17 @@ typedef enum errortype
     ET_MAX
 }
 errortype;
+
+
+/* The structure containing details about each error reported by the mpatrol
+ * library.
+ */
+
+typedef struct errordetails
+{
+    char *code; /* error abbreviation code */
+}
+errordetails;
 
 
 #ifdef __cplusplus
