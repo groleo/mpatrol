ef330d9259fd3c52fd11c6f6e780644aa625a3cc
graeme <graeme@6070c42c-5303-4300-a3c9-e5ce7133e722>
2001-03-07 20:30:03 +0000

    Updated the marked total for reallocations

--- src/info.c
+++ src/info.c
@@ -37,9 +37,9 @@
 
 
 #if MP_IDENT_SUPPORT
-#ident "$Id: info.c,v 1.79 2001-03-07 20:15:43 graeme Exp $"
+#ident "$Id: info.c,v 1.80 2001-03-07 20:30:03 graeme Exp $"
 #else /* MP_IDENT_SUPPORT */
-static MP_CONST MP_VOLATILE char *info_id = "$Id: info.c,v 1.79 2001-03-07 20:15:43 graeme Exp $";
+static MP_CONST MP_VOLATILE char *info_id = "$Id: info.c,v 1.80 2001-03-07 20:30:03 graeme Exp $";
 #endif /* MP_IDENT_SUPPORT */
 
 
@@ -712,6 +712,8 @@ __mp_resizememory(infohead *h, void *p, size_t l, size_t a, alloctype f,
             }
             if (p != NULL)
             {
+                h->mtotal -= d;
+                h->mtotal += l;
                 if (h->ltable.tracing)
                     leaktabentry(h, m, d, 1);
                 if (m->data.flags & FLG_PROFILED)
