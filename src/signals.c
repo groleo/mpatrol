179e71550e6264513a2dbfd337cf1833a90ce4e2
groy <groy@6070c42c-5303-4300-a3c9-e5ce7133e722>
2008-07-17 10:51:56 +0000

    Updated to latest LGPL

--- src/signals.c
+++ src/signals.c
@@ -3,20 +3,17 @@
  * A library for controlling and tracing dynamic memory allocations.
  * Copyright (C) 1997-2008 Graeme S. Roy <graemeroy@users.sourceforge.net>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public
- * License as published by the Free Software Foundation; either
- * version 2 of the License, or (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at         * your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser
+ * General Public License for more details.
  *
- * You should have received a copy of the GNU Library General Public
- * License along with this library; if not, write to the Free
- * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- * MA 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 
@@ -45,9 +42,9 @@
 
 
 #if MP_IDENT_SUPPORT
-#ident "$Id: signals.c,v 1.28 2008-07-16 13:17:35 groy Exp $"
+#ident "$Id: signals.c,v 1.29 2008-07-17 10:51:55 groy Exp $"
 #else /* MP_IDENT_SUPPORT */
-static MP_CONST MP_VOLATILE char *signals_id = "$Id: signals.c,v 1.28 2008-07-16 13:17:35 groy Exp $";
+static MP_CONST MP_VOLATILE char *signals_id = "$Id: signals.c,v 1.29 2008-07-17 10:51:55 groy Exp $";
 #endif /* MP_IDENT_SUPPORT */
 
 
