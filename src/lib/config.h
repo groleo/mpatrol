7c6808b2c2fb2b73bc8b0e924808b431503d3c1c
doursse <doursse@6070c42c-5303-4300-a3c9-e5ce7133e722>
2009-03-27 08:51:07 +0000

    * add support of Motiff / Lesstiff in configure.ac  * remove -xc++ as it breaks compilation  * install header files in $includedir/mpatrol-1    and update pc files to not break compilation    with existing programs

--- src/lib/config.h
+++ src/lib/config.h
@@ -801,20 +801,6 @@
 #endif /* MP_INUSE_SUPPORT */
 
 
-/* Indicates if the mptrace tool should be built with support for a graphical
- * user interface.  This is currently only available on UNIX systems running
- * X that have the Motif or LessTif libraries.
- */
-
-#ifndef MP_GUI_SUPPORT
-#if defined(HAVE_CONFIG_H) && !defined(X_DISPLAY_MISSING)
-#define MP_GUI_SUPPORT 1
-#else /* HAVE_CONFIG_H && X_DISPLAY_MISSING */
-#define MP_GUI_SUPPORT 0
-#endif /* HAVE_CONFIG_H && X_DISPLAY_MISSING */
-#endif /* MP_GUI_SUPPORT */
-
-
 /* Indicates if the C functions defined in malloc.c are to have duplicate
  * functions defined with an alternative name.
  */
